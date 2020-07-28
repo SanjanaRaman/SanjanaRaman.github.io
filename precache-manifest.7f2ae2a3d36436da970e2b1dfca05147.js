ca8f15cb2923ad11fc5b947ccbad31830df3c230
SanjanaRaman <sanjanasraman@gmail.com>
2020-07-27 23:54:51 -0400

    Updates

---
+++ precache-manifest.7f2ae2a3d36436da970e2b1dfca05147.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1c7f45c5284f43ef84f2d9b3fd876e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "950524468acfeb5e1a50",
+    "url": "/static/css/2.acde120e.chunk.css"
+  },
+  {
+    "revision": "21cdaa38931affa759de",
+    "url": "/static/css/main.d5114987.chunk.css"
+  },
+  {
+    "revision": "950524468acfeb5e1a50",
+    "url": "/static/js/2.b7c8da57.chunk.js"
+  },
+  {
+    "revision": "df04310ad48f4f5411320a1942994090",
+    "url": "/static/js/2.b7c8da57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21cdaa38931affa759de",
+    "url": "/static/js/main.d86ba571.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "f8ee25efa9126f2a8282ae2e2915c0a6",
+    "url": "/static/media/apilogo.f8ee25ef.png"
+  },
+  {
+    "revision": "5ecf046def47d92eac72db6f20ad4516",
+    "url": "/static/media/apipic.5ecf046d.png"
+  },
+  {
+    "revision": "be1a8875853618abb842832c09580f38",
+    "url": "/static/media/apipic2.be1a8875.png"
+  },
+  {
+    "revision": "985a10e27246c452dd6192ae0e33fa02",
+    "url": "/static/media/bwphoto.985a10e2.jpg"
+  },
+  {
+    "revision": "a28914327a977446744c49f544fde20f",
+    "url": "/static/media/languagepic.a2891432.png"
+  },
+  {
+    "revision": "c1c1d9ed8b8b0547e8c0d193970a5be0",
+    "url": "/static/media/mobilepraan.c1c1d9ed.png"
+  },
+  {
+    "revision": "0ff4086718cb81e4dcdd9edcbb786c4b",
+    "url": "/static/media/mobileweb.0ff40867.png"
+  },
+  {
+    "revision": "342324b3a5dcd1eab1c16cfa6a498af1",
+    "url": "/static/media/praan.342324b3.png"
+  },
+  {
+    "revision": "02869979f79ff22b9c6075b083315e82",
+    "url": "/static/media/praan1.02869979.png"
+  },
+  {
+    "revision": "103fccd00b0e53a70a1ab8c688a07166",
+    "url": "/static/media/stockimage.103fccd0.png"
+  },
+  {
+    "revision": "6e30874c06a7076bd1c0202979cbb6cf",
+    "url": "/static/media/stockimage2.6e30874c.png"
+  },
+  {
+    "revision": "7f669c083bb69497b75f1fe136cd5c92",
+    "url": "/static/media/webimage.7f669c08.png"
+  }
+]);
